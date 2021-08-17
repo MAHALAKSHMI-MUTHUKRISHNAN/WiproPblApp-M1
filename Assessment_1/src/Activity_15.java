a9d1e5d2cf20c48025db660dbf7135a2ddc9557f
MAHALAKSHMI M <66433024+MAHALAKSHMI-MUTHUKRISHNAN@users.noreply.github.com>
2021-08-17 21:05:25 +0530

    Create Activity_15.java

---
+++ Assessment_1/src/Activity_15.java
@@ -0,0 +1,17 @@
+package demo;
+
+import java.util.Scanner;
+
+public class Activity_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+		    int num;
+		    int rev_num;
+		    int num_diff=36;
+		    int digit_diff = (num_diff) / 9;
+		    System.out.print("The difference between digits of the number is : "+digit_diff);
+	}
+
+}
