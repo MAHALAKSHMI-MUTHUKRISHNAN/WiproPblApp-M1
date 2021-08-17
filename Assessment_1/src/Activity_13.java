3e8a9b5a3802a689970b04a4ffab6c59b48ed943
MAHALAKSHMI M <66433024+MAHALAKSHMI-MUTHUKRISHNAN@users.noreply.github.com>
2021-08-17 20:47:27 +0530

    Update Activity_13.java

--- Assessment_1/src/Activity_13.java
+++ Assessment_1/src/Activity_13.java
@@ -1,8 +1,19 @@
-public class Activity_13{
-public static void main(String args[])
-{
-int l=63;
-int b=40;
-int area=l*b;
-System.out.println("Area of rectangle="+area);
+package demo;
+
+
+public class Activity_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l,b,d=23;
+		int p=206;
+		int s=p/2;
+		l=(s+d)/2;
+		b=l-d;
+		int area=l*b;
+		System.out.println(l+" "+b);
+		System.out.println("Area"+area);
+			
+	}
+
 }
