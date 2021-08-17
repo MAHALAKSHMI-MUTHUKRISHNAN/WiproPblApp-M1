ead7bca0b3c15da6719232c8133e370374187017
MAHALAKSHMI M <66433024+MAHALAKSHMI-MUTHUKRISHNAN@users.noreply.github.com>
2021-08-17 20:22:10 +0530

    Update Activity_10.java

--- Assessment_1/src/Activity_10.java
+++ Assessment_1/src/Activity_10.java
@@ -7,39 +7,16 @@ public class Activity_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int d=21,e=36,f=66,ans=1;
-		lcm l=new lcm();
-		int abc=l.findlcm(d,e,f);
-		int arr[]=new int[d];
-		Arrays.fill(arr, 0);
-		int num=d;int lcm=abc;
-		for(int i=2;i<d&&d>1;i++){
-			while((abc%i)==0){
-		    arr[i]++;abc/=i;
-			}
+		int number;
+		for(int i=21;i<=(int)Math.pow(2,21);i++)
+		{
+		    int square_root=(int)Math.sqrt(i);
+		    if((square_root*square_root == i)&&(i%21==0 && i%36==0 && i%66==0))
+		    {	
+		    System.out.println(i);
+		    break;
+		    }
 		}
-		for(int i=2;i<num;i++){
-			if(arr[i]%2!=0)
-				ans*=i;
-		}
-		System.out.println("The least perfect square number the is divisible by 21,36,66 is: "+ans*lcm);
-	
-	}
-
-}
-class lcm{
-	int gcd(int d, int e)
-	{
-	    if (e == 0)
-	        return d;
-	    return gcd(e, d % e);
-	}
-	int findlcm(int d, int e,int f)
-	{
-	    int ans = d;
-	    ans = ((e * ans) /gcd(e, ans));
-	    ans = ((f* ans) /gcd(f, ans));
-	    return ans;
 	}
 	 
 }
